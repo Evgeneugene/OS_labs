08894022c93ced69792a60d8c5971f51804f1559
Evgeneugene <75316067+Evgeneugene@users.noreply.github.com>
2021-11-03 01:23:11 +0300

    Update ex2.sh

--- week11/ex2.sh
+++ week11/ex2.sh
@@ -1,11 +1,14 @@
-mkdir -p {lofsdisk/bin,lofsdisk/lib64,lofsdisk/usr/lib,lofsdisk/usr/lib64}
+mkdir -p lofsdisk/bin
+mkdir -p lofsdisk/lib64
+mkdir -p lofsdisk/usr/lib
+mkdir -p lofsdisk/usr/lib64
 cp /bin/bash lofsdisk/bin
 ldd /bin/bash | awk '{print $3}' | xargs -I {} cp -v {} lofsdisk{}
 cp lofsdisk/usr/lib/* lofsdisk/lib
 cp lofsdisk/usr/lib64/* lofsdisk/lib64
 echo file1 > lofsdisk/file1
 echo file2 > lofsdisk/file2
-gcc ex2.c -o ex2
-cp ex2 lofsdisk
-sudo chroot lofsdisk /ex2 > ex2.txt
-./ex2 >> ex2.txt
+gcc ex2.c -o ex2.out
+cp ex2.out lofsdisk
+sudo chroot lofsdisk /ex2.out > ex2.txt
+./ex2.out >> ex2.txt
